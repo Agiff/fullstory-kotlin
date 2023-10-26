1d7ab83f04b69e7071fb744cf6de255392ec83f4
Agiff <ghifarimohammadrahadian@gmail.com>
2023-10-27 01:53:02 +0700

    feat: adding pdf viewer

--- app/src/main/java/com/fullstorydev/shoppedemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/fullstorydev/shoppedemo/ui/main/MainFragment.kt
@@ -6,9 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import com.github.barteksc.pdfviewer.PDFView
 import com.devhaus.fullstorypoc.R
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class MainFragment : Fragment() {
+
+    private lateinit var pdfView: PDFView
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,9 +27,43 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.main_fragment, container, false)
+        pdfView = rootView.findViewById(R.id.pdfView)
+
+        val pdfUrl = "https://www.africau.edu/images/default/sample.pdf"
+
+        GlobalScope.launch {
+            val pdfData = downloadPdfFromUrl(pdfUrl)
+            withContext(Dispatchers.Main) {
+                if (pdfData != null) {
+                    pdfView.fromBytes(pdfData.bytes())
+                        .defaultPage(0)
+                        .load()
+                } else {
+                    // Handle download failure
+                }
+            }
+        }
+
+        return rootView
+    }
+
+    private suspend fun downloadPdfFromUrl(url: String) = withContext(Dispatchers.IO) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        val response = client.newCall(request).execute()
+        if (response.isSuccessful) {
+            response.body
+        } else {
+            null
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
